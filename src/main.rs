8d0b8056e91c1a2578700522ac7d112b78fc308b
cloudybyte <cloudybyte@gmx.net>
2020-10-04 19:44:13 +0200

    Upload working

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::io::Write;
+
+use actix_multipart::Multipart;
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use futures::{StreamExt, TryStreamExt};
+use std::path::Path;
+use std::ffi::OsStr;
+use uuid::Uuid;
+
+async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    // iterate over multipart stream
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("./tmp/{}", rewrite_filename(filename));
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+    Ok(HttpResponse::Ok().into())
+}
+
+fn get_extension_from_filename(filename: &str) -> Option<&str> {
+    Path::new(filename)
+        .extension()
+        .and_then(OsStr::to_str)
+}
+
+fn rewrite_filename(filename: &str) -> String {
+    match get_extension_from_filename(filename) {
+        Some(extension) => format!("{}.{}", Uuid::new_v4(), extension),
+        None => Uuid::new_v4().to_string()
+    }
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    std::fs::create_dir_all("./tmp").unwrap();
+
+    let ip = "0.0.0.0:3000";
+
+    HttpServer::new(|| {
+        App::new().wrap(middleware::Logger::default()).service(
+            web::resource("/")
+                .route(web::post().to(save_file)),
+        )
+    })
+        .bind(ip)?
+        .run()
+        .await
+}
