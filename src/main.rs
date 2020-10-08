b98e0ea608b657750f85842ba6f277f74c8d193c
cloudybyte <cloudybyte@gmx.net>
2020-10-08 21:13:00 +0200

    ✨ added info and delete route
    get to `/api` returns info about the api and heliums internal version
    for del explanation see readme

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-use std::io::Write;
+#![allow(unused_variables)]
+#![allow(usued_imports)]
 
-use actix_multipart::Multipart;
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
-use futures::{StreamExt, TryStreamExt};
-use std::path::Path;
 use std::ffi::OsStr;
-use uuid::Uuid;
-use ::s3::creds::{Credentials, AwsCredsError};
+use std::io::Write;
+use std::path::Path;
+
 use ::s3::bucket::Bucket;
+use ::s3::creds::{AwsCredsError, Credentials};
 use ::s3::region::Region;
+use actix_multipart::Multipart;
+use actix_web::{App, Error, HttpResponse, HttpServer, middleware, web};
+use futures::{StreamExt, TryStreamExt};
+use uuid::Uuid;
+
 use crate::util::generate_creds_struct;
+use actix_web::error::ParseError::Header;
+use crate::structs::HeliumConfigWrapper;
+use std::sync::RwLock;
+use config::Config;
 
 mod routes;
 mod s3;
 mod structs;
 mod util;
 mod enums;
+
+
+//Version
+const API_VERSION: &str = "v1";
+const HELIUM_VERSION: &str = "0.2.0";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -27,17 +40,31 @@ async fn main() -> std::io::Result<()> {
     println!("{:?}", &config);
 
     let ip = "0.0.0.0:3000";
+    println!("Listening on {}", ip);
 
-    let config = web::Data::new(util::generate_creds_struct());
+    let config = HeliumConfigWrapper {
+        config,
+        api_version: API_VERSION.to_string(),
+        version: HELIUM_VERSION.to_string()
+    };
+    let config = web::Data::new(config);
+
+
 
     HttpServer::new(move || {
         App::new()
             .app_data(config.clone())
             .wrap(middleware::Logger::default())
+            //upload file
             .service(
-            web::resource("/")
-                .route(web::post().to(routes::upload::save_file)),
-        )
+                web::resource("/")
+                    .route(web::post().to(routes::upload::save_file))
+                    .route(web::delete().to(routes::delete::delete_file))
+            )
+            .service(
+                web::resource("/api")
+                    .route(web::get().to(routes::info::info))
+            )
     })
         .bind(ip)?
         .run()
---
+++ src/routes/info.rs
@@ -0,0 +1,40 @@
+use std::ffi::OsStr;
+use std::io::Write;
+use std::path::Path;
+
+use actix_multipart::{Field, Multipart, MultipartError};
+use actix_web::{Error, HttpRequest, HttpResponse, web};
+use actix_web::body::Body;
+use actix_web::error::PayloadError::Http2Payload;
+use actix_web::http::StatusCode;
+use actix_web::web::Bytes;
+use futures::{FutureExt, StreamExt, TryStreamExt};
+use s3::bucket::Bucket;
+use s3::S3Error;
+use uuid::Uuid;
+use serde::Serialize;
+
+use crate::enums::PermissionLvl;
+use crate::s3::upload::upload_to_s3;
+use crate::s3::util::build_config_struct;
+use crate::structs::{DefaultReturn, DeleteFile, FileUploaded, HeliumConfig, HeliumConfigWrapper};
+use crate::util;
+use crate::util::{build_perm_err, compile_public_url};
+
+
+#[derive(Serialize)]
+struct Info {
+    api_version: String,
+    version: String,
+}
+
+pub async fn info(req: HttpRequest, config: web::Data<HeliumConfigWrapper>) -> HttpResponse<Body> {
+
+    let returnable = Info {
+        api_version: config.api_version.to_owned(),
+        version: config.version.to_owned()
+    };
+
+
+    HttpResponse::Ok().content_type("application/json").body(match serde_json::to_string(&returnable) {Ok(string) => string, Err(err) => format!("an error occured while trying to parse the application information: {:?}", err)})
+}
